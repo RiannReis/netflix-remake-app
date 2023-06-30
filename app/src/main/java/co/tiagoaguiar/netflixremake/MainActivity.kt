654c408ebdf43b4a7fa381ace410cf64000924c5
Riann Silva <ooriannsilva@gmail.com>
2023-06-30 15:39:44 -0300

    Code Cleanup: Separating Classes and Files

--- app/src/main/java/co/tiagoaguiar/netflixremake/MainActivity.kt
+++ app/src/main/java/co/tiagoaguiar/netflixremake/MainActivity.kt
@@ -1,10 +1,7 @@
 package co.tiagoaguiar.netflixremake
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.view.View
-import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
@@ -18,27 +15,6 @@ class MainActivity : AppCompatActivity() {
         val rv: RecyclerView = findViewById(R.id.rv_main)
         rv.layoutManager = LinearLayoutManager(this)
         rv.adapter = adapter
-
-    }
-
-    private inner class MainAdapter : RecyclerView.Adapter<MainAdapter.MovieViewHolder>(){
-
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieViewHolder {
-            val view = layoutInflater.inflate(R.layout.movie_item, parent, false)
-            return MovieViewHolder(view)
-        }
-
-        override fun onBindViewHolder(holder: MovieViewHolder, position: Int) {
-
-        }
-
-        override fun getItemCount(): Int {
-            return 60
-        }
-
-        private inner class MovieViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
-
-        }
 
     }
 
