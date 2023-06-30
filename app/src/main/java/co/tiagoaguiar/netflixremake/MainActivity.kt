e686eb93c7a726dfb2f80da5e470be0b1235c524
Riann Silva <ooriannsilva@gmail.com>
2023-06-30 16:09:00 -0300

    Initial Architecture and Data Models

--- app/src/main/java/co/tiagoaguiar/netflixremake/MainActivity.kt
+++ app/src/main/java/co/tiagoaguiar/netflixremake/MainActivity.kt
@@ -4,14 +4,22 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import co.tiagoaguiar.netflixremake.model.Movie
 
 class MainActivity : AppCompatActivity() {
 
+    // m-v-c (model - [view/controller] activity)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val adapter = MainAdapter()
+        val movies = mutableListOf<Movie>()
+        for (i in 0 until 5){
+            val movie = Movie("https://example.com/$i.jpg")
+            movies.add(movie)
+        }
+
+        val adapter = MainAdapter(movies)
         val rv: RecyclerView = findViewById(R.id.rv_main)
         rv.layoutManager = LinearLayoutManager(this)
         rv.adapter = adapter
