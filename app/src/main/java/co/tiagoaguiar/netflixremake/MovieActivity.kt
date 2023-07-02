38048e32ea8be2f29b5fbbeb19bb6df925581f55
Riann Silva <ooriannsilva@gmail.com>
2023-07-02 14:54:53 -0300

    Prototype: Elegance on the Movie Screen

--- app/src/main/java/co/tiagoaguiar/netflixremake/MovieActivity.kt
+++ app/src/main/java/co/tiagoaguiar/netflixremake/MovieActivity.kt
@@ -18,16 +18,12 @@ class MovieActivity : AppCompatActivity() {
         supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_baseline_arrow_back_24)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
-        //buscar o desenhavel (layer-list)
         val layerDrawable: LayerDrawable = ContextCompat.getDrawable(this, R.drawable.shadows) as LayerDrawable
 
-        //buscar o filme que quer
         val movieCover = ContextCompat.getDrawable(this, R.drawable.movie_4)
 
-        //atribui a esse layer-list esse novo filme
         layerDrawable.setDrawableByLayerId(R.id.cover_drawable, movieCover)
 
-        //set no imageview
         val coverImg: ImageView = findViewById(R.id.movie_img)
         coverImg.setImageDrawable(layerDrawable)
 
