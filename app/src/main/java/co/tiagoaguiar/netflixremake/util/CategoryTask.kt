802b8e769fe4c17a3742fbef62617e4065b17e34
Riann Silva <ooriannsilva@gmail.com>
2023-07-03 15:38:40 -0300

    Parsers: converting data structures

--- app/src/main/java/co/tiagoaguiar/netflixremake/util/CategoryTask.kt
+++ app/src/main/java/co/tiagoaguiar/netflixremake/util/CategoryTask.kt
@@ -1,6 +1,9 @@
 package co.tiagoaguiar.netflixremake.util
 
 import android.util.Log
+import co.tiagoaguiar.netflixremake.model.Category
+import co.tiagoaguiar.netflixremake.model.Movie
+import org.json.JSONObject
 import java.io.BufferedInputStream
 import java.io.ByteArrayOutputStream
 import java.io.IOException
@@ -11,52 +14,91 @@ import javax.net.ssl.HttpsURLConnection
 
 class CategoryTask {
 
-    fun execute(url: String){
+    fun execute(url: String) {
 
         //nesse momento estamos utilizando a UI-Thread (1)
         val executor = Executors.newSingleThreadExecutor()
 
         executor.execute {
+            var urlConnection: HttpsURLConnection? = null
+            var buffer: BufferedInputStream? = null
+            var stream: InputStream? = null
+
+
             try {
                 //nesse momento estamos utilizando a NOVA-Thread [processo paralelo](2)
                 val requestURL = URL(url) //abrir uma URL
-                val urlConnection =
+                urlConnection =
                     requestURL.openConnection() as HttpsURLConnection //abrir a conexão
                 urlConnection.readTimeout = 2000    // tempo de leitura (2s)
                 urlConnection.connectTimeout = 2000 // tempo de conexão (2s)
 
                 val statusCode: Int = urlConnection.responseCode
-                if(statusCode > 400){
+                if (statusCode > 400) {
                     throw IOException("Erro na comunicação com o servidor.")
                 }
 
-                val stream = urlConnection.inputStream //sequência de bytes
+                stream = urlConnection.inputStream //sequência de bytes
 
                 //forma 1: simples e rápida
 //                val jsonAsString = stream.bufferedReader().use { it.readText() }  //bytes -> string
 //                Log.i("teste", jsonAsString)
 
                 //forma 2: bytes -> String
-                val buffer = BufferedInputStream(stream)
+                buffer = BufferedInputStream(stream)
                 val jsonAsString = toString(buffer)
-                Log.i("teste", jsonAsString)
+
+                //o JSON está preparado para ser convertido em um DATA CLASS!
+                val categories = toCategories(jsonAsString)
+                Log.i("teste", categories.toString())
 
 
-            } catch (e: IOException){
+            } catch (e: IOException) {
                 Log.e("teste", e.message ?: "erro desconhecido", e)
+            } finally {
+                urlConnection?.disconnect()
+                stream?.close()
+                buffer?.close()
             }
 
         }
 
     }
 
-    private fun toString(stream: InputStream): String{
+    private fun toCategories(jsonAsString: String): List<Category> {
+        val categories = mutableListOf<Category>()
+
+        val jsonRoot = JSONObject(jsonAsString)
+        val jsonCategories = jsonRoot.getJSONArray("category")
+
+        for (i in 0 until jsonCategories.length()) {
+            val jsonCategory = jsonCategories.getJSONObject(i)
+
+            val title = jsonCategory.getString("title")
+            val jsonMovies = jsonCategory.getJSONArray("movie")
+
+            val movies = mutableListOf<Movie>()
+
+            for (j in 0 until jsonMovies.length()) {
+                val jsonMovie = jsonMovies.getJSONObject(j)
+
+                val id = jsonMovie.getInt("id")
+                val coverUrl = jsonMovie.getString("cover_url")
+
+                movies.add(Movie(id, coverUrl))
+            }
+            categories.add(Category(title, movies))
+        }
+        return categories
+    }
+
+    private fun toString(stream: InputStream): String {
         val bytes = ByteArray(1024)
         val baos = ByteArrayOutputStream()
         var read: Int
-        while (true){
+        while (true) {
             read = stream.read(bytes)
-            if (read <= 0){
+            if (read <= 0) {
                 break
             }
             baos.write(bytes, 0, read)
