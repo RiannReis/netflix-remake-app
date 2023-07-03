4efe91a965fba7c9d9dde289d500df3f4926683f
Riann Silva <ooriannsilva@gmail.com>
2023-07-03 16:52:11 -0300

    Displaying Data in the List

--- app/src/main/java/co/tiagoaguiar/netflixremake/MainActivity.kt
+++ app/src/main/java/co/tiagoaguiar/netflixremake/MainActivity.kt
@@ -1,7 +1,6 @@
 package co.tiagoaguiar.netflixremake
 
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.widget.ProgressBar
 import android.widget.Toast
@@ -12,7 +11,10 @@ import co.tiagoaguiar.netflixremake.model.Category
 import co.tiagoaguiar.netflixremake.util.CategoryTask
 
 class MainActivity : AppCompatActivity(), CategoryTask.Callback {
+
     private lateinit var progress: ProgressBar
+    private lateinit var  adapter: CategoryAdapter
+    private val categories = mutableListOf<Category>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,9 +22,7 @@ class MainActivity : AppCompatActivity(), CategoryTask.Callback {
 
         progress = findViewById(R.id.progress_main)
 
-        val categories = mutableListOf<Category>()
-
-        val adapter = CategoryAdapter(categories)
+        adapter = CategoryAdapter(categories)
         val rv: RecyclerView = findViewById(R.id.rv_main)
         rv.layoutManager = LinearLayoutManager(this)
         rv.adapter = adapter
@@ -37,7 +37,10 @@ class MainActivity : AppCompatActivity(), CategoryTask.Callback {
 
     override fun onResult(categories: List<Category>) {
         //aqui será quando CategoryTask chamará de volta (callback)- listener
-        Log.i("teste MainActivity", categories.toString())
+        this.categories.clear()
+        this.categories.addAll(categories)
+        adapter.notifyDataSetChanged()  //força o adapter a chamar novamente o onBindViewHolder, etc
+
         progress.visibility = View.GONE
 
     }
