6c38b7e3052dd8069050ece75d4d2303f6ee9ba0
Riann Silva <ooriannsilva@gmail.com>
2023-07-05 17:04:26 -0300

    Movie Details

--- app/src/main/java/co/tiagoaguiar/netflixremake/MovieActivity.kt
+++ app/src/main/java/co/tiagoaguiar/netflixremake/MovieActivity.kt
@@ -2,17 +2,21 @@ package co.tiagoaguiar.netflixremake
 
 import android.graphics.drawable.LayerDrawable
 import android.os.Bundle
+import android.util.Log
 import android.view.MenuItem
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import co.tiagoaguiar.netflixremake.model.Movie
+import co.tiagoaguiar.netflixremake.model.MovieDetail
+import co.tiagoaguiar.netflixremake.util.MovieTask
 
-class MovieActivity : AppCompatActivity() {
+class MovieActivity : AppCompatActivity(), MovieTask.Callback {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_movie)
@@ -21,6 +25,12 @@ class MovieActivity : AppCompatActivity() {
         val txtDesc: TextView = findViewById(R.id.movie_txt_desc)
         val txtCast: TextView = findViewById(R.id.movie_txt_cast)
         val rv: RecyclerView = findViewById(R.id.movie_rv_similar)
+
+        val id = intent?.getIntExtra("id", 0) ?: throw IllegalStateException("ID não encontrado")
+
+        val url = "https://api.tiagoaguiar.co/netflixapp/movie/$id?apiKey=26cf6936-a2b8-4ebd-8f9b-095d4d9c7e70"
+
+        MovieTask(this).execute(url)
 
         txtTitle.text = "Batman Begins"
         txtDesc.text = "Essa é a descrição do filme do Batman"
@@ -56,5 +66,17 @@ class MovieActivity : AppCompatActivity() {
         }
 
         return super.onOptionsItemSelected(item)
+    }
+
+    override fun onPreExecute() {
+
+    }
+
+    override fun onResult(movieDetail: MovieDetail) {
+        Log.i("teste", movieDetail.toString())
+    }
+
+    override fun onFailure(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
 }
